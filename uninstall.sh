4227679f8225f43624c1019873af1dc2832d321e
Ibha-X <89918383+Albiahbii@users.noreply.github.com>
2021-12-03 23:44:30 +0530

    uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,34 +1,16 @@
 #!/system/bin/bash
 
-clear
-echo ""
-echo -e "         \e[1;91m__\e[1;92m,-\e[1;93m////\e[1;92m, "
-echo -e "        \e[1;91m/__\e[1;92m) (\e[1;93mo\e[1;92m) ) "
-echo -e "          /.,--. \       "
-echo -e "         /,-'/.\. \      "
-echo -e "         \.  \.\ \ \     "
-echo -e "          \.  \\\ \  /   "
-echo -e "      \e[1;93m=====\e[1;91m((\e[1;93m=\e[1;91m((\e[1;92m\\e[1;93m=====.=== "
-echo -e "           \e[1;92m    \ \ \.\   "
-echo -e "                \ \ \'     "
-echo -e "                 \ \'      "
-echo -e "                  \ \     "
-echo -e "                   '-'    \e[1;97m "
-echo ""
-echo -e "\e[1;91m [+] YouTube: \e[1;92mIbhaX"
-echo -e "\e[1;91m [+] Github: \e[1;92mIbhaX\e[1;97m"
-echo ""
-read -p " Does you want to Uninstall Parrot Shell?(Yes/No) : " input
+read -p " Do you want to Uninstall Ibha Shell?(Yes/No) : " input
 
 if [[ $input == Yes || $input == yes || $input == y || $input == Y ]]; then
     clear
-    cd $HOME/backup
-    mv default bash.bashrc
+    cd
     cd /data/data/com.termux/files/usr/etc
     rm -rf bash.bashrc
-    cd $HOME/backup
+    cd $HOME/Ibha-X/.backup
     mv bash.bashrc /data/data/com.termux/files/usr/etc
     cd $HOME
+    rm -rf Ibha-X
     echo -e "\e[1;91mSuccessfully Uninstalled"
     echo -e "Restart Termux"
     exit  3
