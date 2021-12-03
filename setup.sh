8d10fbfa5338d124c1b10251033ec4f4240acadf
Albiahbii <89918383+Albiahbii@users.noreply.github.com>
2021-12-03 21:08:00 +0530

    setup.sh

--- setup.sh
+++ setup.sh
@@ -19,28 +19,16 @@ for i in "${packages[@]}"; do
     fi
 done
 
-cd 
-mkdir bin
-cd .termux
-rm -rf termux.properties
-cd
-mkdir .termux
-export PATH=~/bin:$PATH
-cd boot-script
-cp termux.properties $HOME/.termux
-cp m1rx ~/../usr/bin
-cd
-cd bin
-chmod +x m1rx
-cd
-source ~/../usr/bin
-
 pip install lolcat
 
-timeout 5s m1rx
+    cd
+    cd IbhaX
+    cp m1rx ~/bin
+    source ~/bin/m1rx
+    timeout 5s m1rx
 
-mkdir tools
-cd tools
+    mkdir tools
+    cd tools
 git clone https://github.com/Albiahbii/cupp
 git clone https://github.com/Albiahbii/XLR8_BOMBER
 git clone https://github.com/albiahbi/TBOMB-version-2.0
@@ -55,7 +43,6 @@ read -p " Do you want to install IbhaX Shell in Termux?(Yes/No) : " input
 if [[ $input == Yes || $input == yes || $input == y || $input == Y ]]; then
     clear
     cd
-    mkdir backup
     cd IbhaX
     cp default $HOME/backup
     cd /data/data/com.termux/files/usr/etc
@@ -65,10 +52,24 @@ if [[ $input == Yes || $input == yes || $input == y || $input == Y ]]; then
     rm -rf bash.bashrc
     cd $HOME/IbhaX
     cp bash.bashrc /data/data/com.termux/files/usr/etc
+    cd 
+    mkdir backup
+    mkdir bin
+    export PATH=~/bin:$PATH
+    cd .termux
+    cp termux.properties $HOME/backup
+    rm -rf $HOME/termux.properties
+    cd
+    cd IbhaX
+    cp termux.properties $HOME/.termux
+    cd
+    clear
     echo -e "\e[1;91mSuccessfully Installed"
     echo -e "Restart Termux to view changes..."
     echo -e ""
+
     exit  3
+
 elif [[ $input == No || $input == no || $input == n || $input == N ]]; then
 exit 2
 else
